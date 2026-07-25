d04dd2d15b06db5d6edbb60c24624c3f977ac126
x0lg0n <kumarsiddharthakain@gmail.com>
2026-07-25 11:15:03 +0530

    revert: restore landing page to original state, remove cinematic landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,30 +1,25 @@
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import LenisProvider from "@/components/landing/LenisProvider";
-import LandingHero from "@/components/landing/LandingHero";
-import TrustedBy from "@/components/landing/TrustedBy";
-import Features from "@/components/landing/Features";
-import HowItWorksSection from "@/components/landing/HowItWorksSection";
-import Testimonials from "@/components/landing/Testimonials";
-import PricingSection from "@/components/landing/PricingSection";
+import Hero from "../components/Hero";
+import ProblemCards from "../components/ProblemCards";
+import Engines from "../components/Engines";
+import WhyChooseUs from "../components/WhyChooseUs";
+import HowItWorks from "../components/HowItWorks";
+import Audience from "../components/Audience";
+import Faq from "../components/Faq";
+import WaitlistCta from "../components/WaitlistCta";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <LenisProvider>
-      <div className="bg-[#081B2E]">
-        <div className="bg-[#081B2E] pt-5">
-          <Navbar />
-        </div>
-        <main>
-          <LandingHero />
-          <TrustedBy />
-          <Features />
-          <HowItWorksSection />
-          <Testimonials />
-          <PricingSection />
-        </main>
-        <Footer />
-      </div>
-    </LenisProvider>
+    <main className="overflow-hidden bg-paper">
+      <Hero />
+      <ProblemCards />
+      <Engines />
+      <WhyChooseUs />
+      <HowItWorks />
+      <Audience />
+      <Faq />
+      <WaitlistCta />
+      <Footer />
+    </main>
   );
 }
